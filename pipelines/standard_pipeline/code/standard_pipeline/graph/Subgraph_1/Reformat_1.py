3765bd99236fb346f9d0d6285393f08ca5dd6f5a
kyakkala@prophecy.io <kyakkala@prophecy.io>
2023-05-14 05:21:59 +0000

    🔁 Modified     - Pipeline        - standard_pipeline     - Subgraph        - revenue_enrich

--- pipelines/standard_pipeline/code/standard_pipeline/graph/Subgraph_1/Reformat_1.py
+++ pipelines/standard_pipeline/code/standard_pipeline/graph/Subgraph_1/Reformat_1.py
@@ -9,6 +9,6 @@ def Reformat_1(spark: SparkSession, in0: DataFrame) -> DataFrame:
     return in0.select(
         col("customer_id"), 
         concat(col("first_name"), lit(", "), col("last_name")).alias("full_name"), 
-        concat(format_number(col("amount"), 2), lit(" $$")).alias("total_expenditure"), 
+        concat(format_number(col("amount"), 2), lit(" $")).alias("total_expenditure"), 
         date_format(col("account_open_date"), "MMM, yyyy").alias("date_of_join")
     )
