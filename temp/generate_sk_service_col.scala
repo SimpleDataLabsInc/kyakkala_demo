af00b6892e8899b2e1c84d1bae4bf9a3a7f1684e
kishoreyakkala <kyakkala@prophecy.io>
2023-11-18 16:10:59 +0530

    test

--- temp/generate_sk_service_col.scala
+++ temp/generate_sk_service_col.scala
@@ -32,8 +32,6 @@ object generate_sk_service_col {
     import pureconfig.generic.auto._
     import scala.language.implicitConversions
 
-    implicit val formats = DefaultFormats
-
     import com.databricks.dbutils_v1.DBUtilsHolder.dbutils
 
     val api_header_key =
@@ -42,30 +40,27 @@ object generate_sk_service_col {
     case class PkSkGen(pks_sks: String, sk_value: String, p_flag: String)
 
     println("inside generate get_sk...") //todo
-    case class SKDef(
-                      tableName: String,
-                      skCol: String,
-                      nkCol: List[String]
-                    )
-
-    case class SKService(
-                          sks: List[SKDef]
-                        )
+    case class SKDef(tableName: String, skCol: String, nkCol: List[String])
+    case class SKService(sks: List[SKDef])
 
     //case class PkSkGen(pks_sks: String, sk_value: String, p_flag: String)
 
     def jsonStrToMap(jsonStr: String): Map[String, Any] = {
+      implicit val formats = DefaultFormats
       parse(jsonStr).extract[Map[String, Any]]
     }
 
     def get_sk_values_response(
+                                pipeline_name: String,
                                 pk_to_find: List[String],
                                 post_url: String
                               ): String = {
+
+      implicit val formats = DefaultFormats
       val req =
         Map(
-          "client_id" -> Config.pipeline_name,
-          "request_id" -> (Config.pipeline_name + "_" + java.time.LocalDateTime.now.toString),
+          "client_id" -> pipeline_name,
+          "request_id" -> (pipeline_name + "_" + java.time.LocalDateTime.now.toString),
           "pks" -> pk_to_find,
           "detailed" -> "true"
         )
@@ -85,7 +80,7 @@ object generate_sk_service_col {
         println("Partial results got returned. Please connect with admin and check SK service memory. It should be less than 70 percent.")
       }else if (status_code == 429) {
         Thread.sleep(60000)
-        get_sk_values_response(pk_to_find, post_url)
+        get_sk_values_response(pipeline_name, pk_to_find, post_url)
       }
 
       val entity = response.getEntity
@@ -115,11 +110,11 @@ object generate_sk_service_col {
 
     val out0 =
       if (Config.enable_sk_service != "false" && spark.conf.get("new_data_flag") == "true" && Config.sk_service_col != "None") {
-          val sksConfig = SKDef(
-            tableName = Config.target_table,
-            skCol = Config.sk_service_col,
-            nkCol = Config.primary_key.split(",").map(x => x.trim()).toList
-          )
+        val sksConfig = SKDef(
+          tableName = Config.target_table,
+          skCol = Config.sk_service_col,
+          nkCol = Config.primary_key.split(",").map(x => x.trim()).toList
+        )
 
         println("sksConfig.tableName: " + sksConfig.tableName)
         val temp_df = in0
@@ -132,42 +127,53 @@ object generate_sk_service_col {
           ).dropDuplicates()
 
         val post_url = Config.sk_service_base_url + sksConfig.tableName + "/get_sks"
-        val sk_outputs = temp_df.rdd.mapPartitions { rowPartition =>
-          rowPartition.grouped(Config.sk_service_batch_size)
-            .flatMap { rowGroup =>
-              val sk_input = rowGroup.map(_.getString(0)).toList
-              var sk_response_output = get_sk_values(
-                get_sk_values_response(sk_input, post_url),
-                post_url
-              )
+        val sk_service_batch_size = Config.sk_service_batch_size
+        val pipeline_name = Config.pipeline_name
+        val partitionCount = temp_df.rdd.getNumPartitions
+
+        val sk_outputs_parts = (0 until partitionCount).map{i =>
+          print(s"Generating sks for partition:$i/$partitionCount")
+          temp_df.rdd.mapPartitionsWithIndex{(idx, partitionRows) =>
+            if(i == idx) {
               var while_count = 0
-              while (sk_response_output.length != sk_input.length && while_count < 5) {
-                println("sk_response_output length: " + sk_response_output.length)
-                println("sk_input length: " + sk_input.length)
-                Thread.sleep(61000)
-                while_count = while_count + 1
-                println("while_count: " + while_count)
-                sk_response_output = get_sk_values(
-                  get_sk_values_response(sk_input, post_url),
-                  post_url
-                )
-                if (while_count == 3) {
-                  throw new Exception(
-                    "Please check SK service. Input response size and output response size did not matched in the response after 3 retries with 60 sec gap. Please connect with admin and see memory usaage for redis cache and clear it in case it is more that 70 percent."
+              partitionRows.grouped(sk_service_batch_size)
+                .flatMap { rowGroup =>
+                  val sk_input = rowGroup.map(_.getString(0)).toList
+                  var sk_response_output = get_sk_values(
+                    get_sk_values_response(pipeline_name, sk_input, post_url),
+                    post_url
                   )
+                  while (sk_response_output.length != sk_input.length && while_count < 5) {
+                    println("sk_response_output length: " + sk_response_output.length)
+                    println("sk_input length: " + sk_input.length)
+                    Thread.sleep(61000)
+                    while_count = while_count + 1
+                    println("while_count: " + while_count)
+                    sk_response_output = get_sk_values(
+                      get_sk_values_response(pipeline_name, sk_input, post_url),
+                      post_url
+                    )
+                    if (while_count == 3) {
+                      throw new Exception(
+                        "Please check SK service. Input response size and output response size did not matched in the response after 3 retries with 60 sec gap. Please connect with admin and see memory usaage for redis cache and clear it in case it is more that 70 percent."
+                      )
+                    }
+                  }
+                  sk_response_output.map(res => (res.pks_sks, res.sk_value, res.p_flag))
                 }
-              }
-              sk_response_output.map(res => (res.pks_sks, res.sk_value, res.p_flag))
-            }
-          }.toDF("concat_pk", "service_sk", "placeholder_flag")
+            } else Iterator.empty[(String, String, String)]
+          }
+        }
+
+        val sk_outputs = sk_outputs_parts.reduceLeft(_ union _).toDF("concat_pk", "service_sk", "placeholder_flag")
 
         val helper_col_pf = sksConfig.tableName + "_pf_" + sksConfig.skCol
         val helper_col_sk = sksConfig.tableName + "_sk_" + sksConfig.skCol
         val helper_col_nk = sksConfig.tableName + "_nk_" + sksConfig.skCol
         in0.withColumn(
-            sksConfig.tableName + "_concat_pk",
-            concat_ws("~|~", sksConfig.nkCol.map { nk_col => expr(nk_col) }: _*)
-          ).as("in0")
+          sksConfig.tableName + "_concat_pk",
+          concat_ws("~|~", sksConfig.nkCol.map { nk_col => expr(nk_col) }: _*)
+        ).as("in0")
           .join(
             sk_outputs.as("in1"),
             expr("in0." + sksConfig.tableName + "_concat_pk == in1.concat_pk"),
